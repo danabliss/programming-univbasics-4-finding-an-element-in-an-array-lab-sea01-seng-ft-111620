d9484f80c807d9ceccb4049fe437284c3c04217a
Dana Jackson <danajackson2@gmail.com>
2020-10-13 15:55:23 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   i = 0
   while array.length<i do
-    array.include?(value_to_find)
-    
+    if array[i] == value_to_find
+      found_value_index = i
     i += 1
   end  
+  found_value_index
 end
